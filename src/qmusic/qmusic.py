664e38fc2d6a38adf141daeba7ae2d02517f019c
jwitty3 <66694672+jwitty3@users.noreply.github.com>
2022-06-29 12:08:25 -0600

    Improved UI
    Fixed Windows display issue, added volume slider, added input validation

--- src/qmusic/qmusic.py
+++ src/qmusic/qmusic.py
@@ -10,11 +10,15 @@ from  scipy import signal
 import pygame
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 import os
+import re
+import ctypes
+import platform
 
 def main():
-    
-    global notes, bassline, scales, states, root, sounds, afters, basssounds, bassbox, loopbox, fig, axes, canvas
+    axes = None
+    sounds = []
     #frequencies of notes
     notes = np.array([
         261.63, #C        0
@@ -47,60 +51,61 @@ def main():
     ], dtype=np.ndarray)
 
     states = [coherent(20, 1.73)]
-
+    
+    if (platform.system() == "Windows"):
+        awareness = ctypes.c_int()
+        errorCode = ctypes.windll.shcore.GetProcessDpiAwareness(0, ctypes.byref(awareness))
+        errorCode = ctypes.windll.shcore.SetProcessDpiAwareness(2)
+        success = ctypes.windll.user32.SetProcessDPIAware()
+    
     root = Tk()
     root.title('Quantum Music')
-
+    
     pygame.mixer.init(size=32)
-    sounds = []
-    afters = []
-    basssounds = []
     bassbox = IntVar()
     loopbox = IntVar()
-
-
+    volume = 1.0;
+    
     #generate data array by measuring a state
     def generate():
-        dim = int(bignvalue.get())
-        littlen = int(littlenvalue.get())
+        scale = []
+        if scalebox.current() == -1:
+            test = "^(([0-9]|1[01])\,[\s]*)+([0-9]|1[01])?\s*$"
+            val = scalebox.get() + " "
+            if re.sub(test, "", val) == "":
+                scale = np.fromstring(val, dtype=int, sep=",")
+            else:
+                messagebox.showerror('Input Error', "'Scale' input must be a list of integers less than or equal to 11")
+                return
+        else:
+            scale = np.array(scales[scalebox.current()])
+        dim = 20 if bignvalue.get() == "" else int(bignvalue.get())
+        littlen = 0 if littlenvalue.get() == "" else int(littlenvalue.get())
 
-        global states
-        states = [coherent(dim, np.sqrt(littlen)), basis(dim, n=littlen), fock(dim, n=littlen), thermal_dm(dim, littlen), maximally_mixed_dm(dim), phase_basis(dim, littlen), projection(dim, 2, 2)]
+        states = [coherent(dim, np.sqrt(littlen)), basis(dim, n=littlen), fock(dim, n=littlen), thermal_dm(dim, littlen), maximally_mixed_dm(dim)]
         operators = [create(dim),destroy(dim),displace(dim, 1),qeye(dim),momentum(dim),num(dim),phase(dim),position(dim),qzero(dim),tunneling(dim)]
         data = np.array([])
 
-        graph()
-
+        graph(states)
+        
+        
         for i in range(int(length.get())):
             x = qm.measure(states[int(state.current())], operators[int(operator.current())])
             x = int(str(x[1].data)[3:4])
             data = np.append(data, x)
 
-        sound(data)
+        generatesound(data, scale)
 
     #play sounds
-    def sound(state):
-        cancelsound()
-
+    def generatesound(state, scale):
         playbutton.configure(command=cancelsound)
         playbutton['text'] = "Stop"
-
+        
         bassindex = 0;
         bpm = int(speed.get())
-        dur = 2.0
         fs = 44100
-        global sounds
-        global afters
-        global basssounds
-        sounds = []
-        afters = []
-        basssounds = []
         freqs = np.array([0])
         #indices of notes in notes array
-        if scalebox.current() == -1:
-            scale = np.fromstring(scalebox.get(), dtype=int, sep=",")
-        else:
-            scale = np.array(scales[scalebox.current()])
 
         #generate array of note frequencies in selected scale
         for i in range(int(max(state) + 1)):
@@ -108,87 +113,97 @@ def main():
             freqs = np.append(freqs, freq)
 
         #generate sounds using pygame sounds
+        buffer = []
         for index in range(state.size):
             #to change the wave in the buffer variable, use "signal.square" or "np.sine" (or maybe "signal.sweep.poly") or "signal.sawtooth"
-            t = 2 * np.pi * np.arange(fs * dur * 60 / bpm)
-            buffer = signal.sawtooth(t * freqs[int(state[index])] / fs).astype(np.float32)
-            sound = pygame.mixer.Sound(buffer)
-            sounds.append(sound)
-            afters.append(1)
-
+            t = 2 * np.pi * np.arange(fs * 60 / bpm)
+            buffer = np.concatenate((buffer, signal.sawtooth(t * freqs[int(state[index])] / fs).astype(np.float32)))
+        
         #generate bassline
+        bassbuffer = []
         for index in range(int(bassline.size / 4)):
             bassarr = bassline[index]
-            y = 2 * np.pi * np.arange(fs * bassarr[1] * 2 * 60 / bpm)
-            buffer = np.sin(y * notes[int(bassarr[2])] * bassarr[3] / fs).astype(np.float32)
-            bassbuffer = pygame.mixer.Sound(buffer)
-            basssounds.append(bassbuffer)
+            y = 2 * np.pi * np.arange(fs * bassarr[1] * 60 / bpm)
+            bassbuffer = np.concatenate((bassbuffer, np.sin(y * notes[int(bassarr[2])] * bassarr[3] / fs).astype(np.float32)))
             bassindex += 1
-
-        interval = int(sounds[0].get_length() * 1000)
-
-        #play sounds using recursive tkinter after()
-        def playbass(bassind):
-            basssounds[bassind].play()
-            if bassind < len(basssounds) - 1:
-                basssounds[bassind] = root.after(interval * int(bassline[bassind][0]), lambda : playbass(bassind + 1))
-
-        def playnotes(ind):
-            sounds[ind].play()
-            if ind < len(sounds) - 1:
-                afters[ind] = root.after(interval, lambda : playnotes(ind + 1))
-            elif int(loopbox.get()) == 1:
-                playnotes(0)
-            else:
-                cancelsound()
-
-        playnotes(0)
-        if int(bassbox.get()) == 1:
-            playbass(0)
-
+        
+        basssound = pygame.mixer.Sound(bassbuffer)
+        sound = pygame.mixer.Sound(buffer)
+        sounds.append(sound)
+        sounds.append(basssound)
+        setvolume(sounds)
+        def playsound():
+            if (playbutton['text'] == "Play"):
+                return
+            sound.play()
+            content.after(int(sound.get_length() * 1000), lambda: playsound() if loopbox.get() == 1 else cancelsound())       
+        playsound()
+        if (bassbox.get() == 1):
+            basssound.play()
+        
     #stop playing sounds
     def cancelsound():
-
         playbutton.configure(command=generate)
         playbutton['text'] = "Play"
-
-        #get arrays of after objects
-        global afters
-        global basssounds
-
-        #cancel all after objects
-        for i in afters:
-            root.after_cancel(i)
-        for i in basssounds:
-            root.after_cancel(i)
-
-    #build widgets
+        pygame.mixer.stop()
+        
+    def setvolume(obj):
+        for sound in sounds:
+            sound.set_volume(1 / (10 - volumeslider.get()) - .1)
+    
+    def validatenum(input, max, message):
+        if max == "bignvalue":
+            max = 19 if bignvalue.get() == "" else int(bignvalue.get()) - 1
+        if input == "":
+            return True
+        if input.isdigit():
+            if int(input) <= int(max):
+                return True
+            else:
+                messagebox.showerror('Input Out of Bounds', message)
+        return False
+    
+    def validatescale(input):
+        test = "^[\,\s0-9]*$"
+        if re.sub(test, "", input) == "":
+            return True
+        return False
+    
+    #build widgetsa
     content = ttk.Frame(root)
     labels = ttk.Frame(content)
     inputs = ttk.Frame(content)
+    playcontainer = ttk.Frame(content)
+    
+    #validate command
+    vcmd = inputs.register(validatenum);
+    vcmds = inputs.register(validatescale);
+    
     title = ttk.Label(content, text="Quantum Music")
     bignvaluelabel = ttk.Label(labels, text="N")
     littlenvaluelabel = ttk.Label(labels, text="n")
     statelabel = ttk.Label(labels, text="State")
     operatorlabel = ttk.Label(labels, text="Operator")
     lengthlabel = ttk.Label(labels, text="Music Length")
-    speedlabel = ttk.Label(labels, text="Speed")
+    speedlabel = ttk.Label(labels, text="Speed (BPM)")
     basslabel = ttk.Label(labels, text="Bassline")
     looplabel = ttk.Label(labels, text="Loop")
     scalelabel = ttk.Label(labels, text="Scale")
-    bignvalue = ttk.Entry(inputs)
-    littlenvalue = ttk.Entry(inputs)
-    length = ttk.Entry(inputs)
-    speed = ttk.Entry(inputs)
+    bignvalue = ttk.Entry(inputs, validate = "key", validatecommand = (vcmd, '%P', 100, "N valaue must be an integer less than 100"))
+    littlenvalue = ttk.Entry(inputs, validate = "key", validatecommand = (vcmd, '%P', "bignvalue", "n value must be an integer less than N"))
+    length = ttk.Entry(inputs, validate = "key", validatecommand = (vcmd, '%P', 385, "Length value must be an integer less than 385"))
+    speed = ttk.Entry(inputs, validate = "key", validatecommand = (vcmd, '%P', 1000, "Speed value must be an integer less than 1000"))
     bass = ttk.Checkbutton(inputs, variable=bassbox, onvalue=1, offvalue=0)
     loop = ttk.Checkbutton(inputs, variable=loopbox, onvalue=1, offvalue=0)
-    scalebox = ttk.Combobox(inputs)
+    scalebox = ttk.Combobox(inputs, validate = "key", validatecommand = (vcmds, '%P'))
     scalebox['values'] = ('C Minor Pentatonic', 'C Major', 'C Blues')
     state = ttk.Combobox(inputs)
-    state['values'] = ('Coherent', 'Basis', 'Fock', 'Thermal', 'maximally_mixed', 'phase_basis', 'projection')
+    state['values'] = ('Coherent', 'Basis', 'Fock', 'Thermal', 'Maximally Mixed')
     operator = ttk.Combobox(inputs)
     operator['values'] = ('Create', 'Destroy', 'Displace', 'Identity', 'Momentum', 'Num', 'Phase', 'Position', 'Zero', 'Tunneling')
-    playbutton = ttk.Button(content, text="Play", command=generate)
+    playbutton = ttk.Button(playcontainer, text="Play", command=generate)
+    volumeslider = ttk.Scale(playcontainer, from_=0, to=9.09, orient="horizontal", value=9.09, command=lambda x: setvolume(sounds))
+    volumelabel = ttk.Label(playcontainer, text="Volume")
 
     bignvalue.insert(0, "20")
     littlenvalue.insert(0, "3")
@@ -202,32 +217,35 @@ def main():
     operator.state(["readonly"])
 
     style = ttk.Style(content)
-    style.configure("TLabel", font=("Arial", 13))
+    style.configure("TLabel", font=("Arial", 12))
 
     #display widgets
     content.pack(fill="both", expand=True)
     title.pack(pady=20)
-    playbutton.pack(side="bottom", expand=True, pady=20, anchor="n")
-    labels.pack(side="left", fill="both", expand=True)
-    inputs.pack(side="left", fill="both", expand=True)
-    bignvalue.pack(expand=True, anchor="e", padx=20, pady=10)
-    littlenvalue.pack(expand=True, anchor="e", padx=20, pady=10)
-    length.pack(expand=True, anchor="e", padx=20, pady=10)
-    speed.pack(expand=True, anchor="e", padx=20, pady=10)
-    bass.pack(expand=True, anchor="e", padx=20, pady=10)
-    loop.pack(expand=True, anchor="e", padx=20, pady=10)
-    scalebox.pack(expand=True, anchor="e", padx=20, pady=10)
-    state.pack(expand=True, anchor="e", padx=20, pady=10)
-    operator.pack(expand=True, anchor="e", padx=20, pady=10)
-    bignvaluelabel.pack(expand=True, anchor="e", padx=20, pady=10)
-    littlenvaluelabel.pack(expand=True, anchor="e", padx=20, pady=10)
-    lengthlabel.pack(expand=True, anchor="e", padx=20, pady=10)
-    speedlabel.pack(expand=True, anchor="e", padx=20, pady=10)
-    basslabel.pack(expand=True, anchor="e", padx=20, pady=10)
-    looplabel.pack(expand=True, anchor="e", padx=20, pady=10)
-    scalelabel.pack(expand=True, anchor="e", padx=20, pady=10)
-    statelabel.pack(expand=True, anchor="e", padx=20, pady=10)
-    operatorlabel.pack(expand=True, anchor="e", padx=20, pady=10)
+    playcontainer.pack(side="bottom", expand=True, pady=25)
+    playbutton.pack(side="left", expand=True, padx=25)
+    volumelabel.pack(side="left", expand=True, padx=10)
+    volumeslider.pack(side="left", expand=True)
+    labels.pack(side="left", fill="both", expand=True, padx=20)
+    inputs.pack(side="left", fill="both", expand=True, padx=20)
+    bignvalue.pack(expand=True, anchor="e", pady=10)
+    littlenvalue.pack(expand=True, anchor="e", pady=10)
+    length.pack(expand=True, anchor="e", pady=10)
+    speed.pack(expand=True, anchor="e", pady=10)
+    bass.pack(expand=True, anchor="e", pady=10)
+    loop.pack(expand=True, anchor="e", pady=10)
+    scalebox.pack(expand=True, anchor="e", pady=10)
+    state.pack(expand=True, anchor="e", pady=10)
+    operator.pack(expand=True, anchor="e", pady=10)
+    bignvaluelabel.pack(expand=True, anchor="e", pady=10)
+    littlenvaluelabel.pack(expand=True, anchor="e", pady=10)
+    lengthlabel.pack(expand=True, anchor="e", pady=10)
+    speedlabel.pack(expand=True, anchor="e", pady=10)
+    basslabel.pack(expand=True, anchor="e", pady=10)
+    looplabel.pack(expand=True, anchor="e", pady=10)
+    scalelabel.pack(expand=True, anchor="e", pady=10)
+    statelabel.pack(expand=True, anchor="e", pady=10)
+    operatorlabel.pack(expand=True, anchor="e", pady=10)
     
     fig, axes = plt.subplots(figsize=(5, 5))
     plot_fock_distribution(states[int(state.current())], fig=fig, ax=axes)
@@ -235,17 +253,14 @@ def main():
     canvas.draw()
     canvas.get_tk_widget().pack(fill="both", expand=True, padx=20)
 
-    def graph():
-        global fig
-        global axes
-        global canvas
-
+    def graph(states):
         axes.clear()
         plot_fock_distribution(states[int(state.current())], fig=fig, ax=axes)
         canvas.draw()
     
     def close():
         root.destroy()
+        quit()
     
     root.protocol("WM_DELETE_WINDOW", close)
     
